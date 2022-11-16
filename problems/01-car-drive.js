4f7739345899871e4071099a68ab2ace17ce2107
anton <antonhdo@gmail.com>
2022-11-16 12:49:19 -0800

    first

---
+++ problems/01-car-drive.js
@@ -0,0 +1,17 @@
+class Car {
+	constructor(speed) {
+		this.speed = speed
+	}
+	drive(speed) {
+		return this.speed = speed
+	}
+}
+
+/*****************************************************************************/
+/***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
+
+try {
+	module.exports = Car;
+} catch {
+	module.exports = null;
+}
---
+++ problems/06-fancy-calculator.js
@@ -0,0 +1,12 @@
+class FancyCalculator {
+
+}
+
+/*****************************************************************************/
+/***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
+
+try {
+	module.exports = FancyCalculator;
+} catch {
+	module.exports = null;
+}
